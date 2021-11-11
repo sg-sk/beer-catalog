20a4dd7c15f7a00a3a42ee7867ac8fdbdc5b5f2d
Stephen <sk.gaming.1337@protonmail.com>
2021-11-11 16:46:02 -0500

    Update API query

--- src/API.js
+++ src/API.js
@@ -2,7 +2,7 @@ import { API_URL } from './config';
 
 export async function getBeers(page) {
   try {
-    return await (await fetch(`${API_URL}?page=${page}&per_page=80`)).json();
+    return await (await fetch(`${API_URL}?page=${page}`)).json();
   } catch (error) {
     console.log(error);
   }
