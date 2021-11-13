448ee65692e7fadb51d707706162c6b6edd24e15
Stephen <sk.gaming.1337@protonmail.com>
2021-11-12 19:18:25 -0500

    Update props

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
-const Product = ({ imageUrl, title, description }) => (
+const Product = ({ imageUrl, title }) => (
   <div>
     <img src={imageUrl} alt='beer' />
     <h3>{title}</h3>
-    <p>{description}</p>
   </div>
 );
 
