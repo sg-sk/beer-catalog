063291f8683959ad6fd4d574b1e4f43fd0e9aca1
Stephen <sk.gaming.1337@protonmail.com>
2021-11-13 22:28:33 -0500

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Home from './components/Home';
+import Beer from './components/Beer';
 
 function App() {
   return (
-    <div className='App'>
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/:beerId' element={<Beer />} />
+      </Routes>
+    </Router>
   );
 }
 
