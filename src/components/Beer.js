71c06b3ba37770ab8da22cdb06f2cc0afc907720
Stephen <sk.gaming.1337@protonmail.com>
2021-11-15 15:38:29 -0500

    Add hook

--- src/components/Beer.js
+++ src/components/Beer.js
@@ -1,25 +1,28 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
 import { getBeer } from '../API';
 
 const Beer = () => {
   const { beerId } = useParams();
-
-  // Fetch beer information
-  const fetchBeer = async () => {
-    try {
-      const beer = await getBeer(beerId);
-      console.log(beer);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const [currentBeer, setCurrentBeer] = useState([]);
+  const [error, setError] = useState(false);
 
   // Initial loading
   useEffect(() => {
+    // Fetch beer information
+    const fetchBeer = async () => {
+      try {
+        setError(false);
+        const beer = await getBeer(beerId);
+        setCurrentBeer(beer);
+        console.log(beer);
+      } catch (error) {
+        setError(true);
+      }
+    };
     fetchBeer();
-  });
+  }, [beerId]);
 
   return <></>;
 };
