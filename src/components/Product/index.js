f1e403616376cb9ec61f8a9fc27f4227390fb803
Stephen <sk.gaming.1337@protonmail.com>
2021-11-15 16:44:00 -0500

    Add link to open single product information page and styling

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const Product = ({ imageUrl, title }) => (
+import { Link } from 'react-router-dom';
+
+const Product = ({ beerId, imageUrl, title, alcoholByVolume }) => (
   <div>
-    <img src={imageUrl} alt='beer' />
-    <h3>{title}</h3>
+    <Link to={`/${beerId}`}>
+      <div className='bg-gray-100 py-11 transition duration-150 ease-in-out transform hover:scale-105'>
+        <img
+          src={imageUrl}
+          alt='beer-product-img'
+          className='flex h-48 w-full object-scale-down'
+        />
+      </div>
+    </Link>
+    <div className='mt-3'>
+      <h3 className='font-bold'>{title}</h3>
+      <p>{alcoholByVolume} ABV</p>
+    </div>
   </div>
 );
 
