4f382f192303bdf358db49f8314c91824d27fa8f
Stephen <sk.gaming.1337@protonmail.com>
2021-11-15 16:48:08 -0500

    Add styling

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Grid = ({ children }) => (
-  <div>
-    <div>{children}</div>
+  <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6'>
+    {children}
   </div>
 );
 
