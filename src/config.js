436190fb54e144205152946551c12f9c6b4aeb54
Stephen <sk.gaming.1337@protonmail.com>
2021-11-15 22:45:02 -0500

    Add links to config

--- src/config.js
+++ src/config.js
@@ -1,2 +1,5 @@
 // Find out more at https://punkapi.com/documentation/v2
 export const API_URL = 'https://api.punkapi.com/v2/beers';
+
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/stephen-kwan/';
+export const GITHUB_URL = 'https://github.com/sg-sk';
