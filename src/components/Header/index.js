94235c50e984402930b28f66f5e5ba1a40ab9482
Stephen <sk.gaming.1337@protonmail.com>
2021-11-15 22:06:12 -0500

    Add header link with logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Header = () => {};
+import Logo from '../../assets/logo.png';
+
+const Header = () => (
+  <div className='py-10'>
+    <Link to='/' className='inline-block'>
+      <img src={Logo} alt='logo' className='h-24' />
+    </Link>
+  </div>
+);
 
 export default Header;
