7b7b38cccd1d5b9672646624d9014e2c7e4de760
Stephen <sk.gaming.1337@protonmail.com>
2021-11-15 23:08:02 -0500

    Add comment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 // Find out more at https://punkapi.com/documentation/v2
 export const API_URL = 'https://api.punkapi.com/v2/beers';
 
+// Social media links
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/stephen-kwan/';
 export const GITHUB_URL = 'https://github.com/sg-sk';
