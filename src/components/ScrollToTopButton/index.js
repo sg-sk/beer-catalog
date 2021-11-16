99e9a8ae6344b58f9627bd1514bf4b79c5961db5
Stephen <sk.gaming.1337@protonmail.com>
2021-11-15 22:06:41 -0500

    Add scroll to top functionality and styling

--- src/components/ScrollToTopButton/index.js
+++ src/components/ScrollToTopButton/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
-import BackToTopArrow from '../assets/chevron-circle-up-solid.svg';
+import BackToTopArrow from '../../assets/chevron-circle-up-solid.svg';
 
-const ScrollToTopButton = () => {};
+const ScrollToTopButton = () => (
+  <button
+    id='scrollToTopButton'
+    type='button'
+    className='fixed bottom-8 right-4'
+    onClick={() => {
+      document.documentElement.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    }}
+  >
+    <img src={BackToTopArrow} alt='back-to-top' className='h-10 w-10' />
+  </button>
+);
 
 export default ScrollToTopButton;
