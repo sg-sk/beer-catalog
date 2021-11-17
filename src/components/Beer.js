49c26effef279aa5c58aa09285bf177aede163ac
Stephen <sk.gaming.1337@protonmail.com>
2021-11-16 22:19:08 -0500

    Add components and styling

--- src/components/Beer.js
+++ src/components/Beer.js
@@ -3,6 +3,13 @@ import { useParams } from 'react-router';
 
 import { getBeer } from '../API';
 
+import NoImageAvailable from '../assets/no-image-available.jpg';
+
+import Header from './Header';
+import Footer from './Footer';
+import Breadcrumb from './Breadcrumb';
+import ProductInfo from './ProductInfo';
+
 const Beer = () => {
   const { beerId } = useParams();
   const [currentBeer, setCurrentBeer] = useState([]);
@@ -24,7 +31,32 @@ const Beer = () => {
     fetchBeer();
   }, [beerId]);
 
-  return <></>;
+  return (
+    <>
+      <div className='mx-7 mb-20 xl:mx-32 2xl:mx-52'>
+        <Header />
+        {error && <div>Oops something happened!</div>}
+        <div>
+          {currentBeer.map((beer) => (
+            <div key={beer.id}>
+              <div className='mb-10'>
+                <Breadcrumb beerName={beer.name} />
+              </div>
+              <ProductInfo
+                imageUrl={beer.image_url ? beer.image_url : NoImageAvailable}
+                name={beer.name}
+                tagline={beer.tagline}
+                description={beer.description}
+                alcoholByVolume={beer.abv}
+                firstBrewed={beer.first_brewed}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default Beer;
