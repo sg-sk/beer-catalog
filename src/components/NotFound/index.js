70b1f7e0561aa3593608de3651e177fcd814df0d
Stephen <sk.gaming.1337@protonmail.com>
2021-11-17 23:19:15 -0500

    Add styling and logic

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,5 +1,24 @@
 import React from 'react';
 
-const NotFound = () => {};
+import NotFoundImage from '../../assets/not-found.gif';
+
+const NotFound = () => {
+  return (
+    <div className='flex flex-col gap-5'>
+      <div className='flex justify-center'>
+        <h1 className='font-bold text-base md:text-2xl'>
+          We couldn't find what you were looking for 😥
+        </h1>
+      </div>
+      <div className='flex justify-center'>
+        <img
+          src={NotFoundImage}
+          alt='not-found'
+          className='h-60 md:h-96 w-full object-scale-down'
+        />
+      </div>
+    </div>
+  );
+};
 
 export default NotFound;
