04e56699a08673156b72e374a05510732b84f393
Stephen <sk.gaming.1337@protonmail.com>
2021-11-21 18:03:06 -0500

    Add API pagination static variable

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 // Find out more at https://punkapi.com/documentation/v2
 export const API_URL = 'https://api.punkapi.com/v2/beers';
+export const API_PAGINATION_ITEMS = 25;
 
 // Social media links
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/stephen-kwan/';
