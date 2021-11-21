6bd78e6fa01e8732c098d32971d1d6c96456ab78
Stephen <sk.gaming.1337@protonmail.com>
2021-11-21 17:44:26 -0500

    Add SearchBar logic and styling

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
-const SearchBar = ({ searchTerm }) => {};
+const SearchBar = ({ setSearchTerm }) => {
+  const initialRender = useRef(true);
+  const [state, setState] = useState('');
+
+  useEffect(() => {
+    // Skip initial useEffect render
+    if (initialRender.current) {
+      initialRender.current = false;
+      return;
+    }
+
+    const timer = setTimeout(() => {
+      setSearchTerm(state);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [setSearchTerm, state]);
+
+  return (
+    <div className='flex gap-4'>
+      <input
+        type='text'
+        placeholder='Search Beer 🍺'
+        onChange={(event) => setState(event.currentTarget.value)}
+        value={state}
+        className='py-3 px-4 text-xl text-center w-full rounded bg-gray-100 focus:outline-none'
+      />
+    </div>
+  );
+};
 
 export default SearchBar;
