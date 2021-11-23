d20a8ef7c9d18481cdd73503ded95362017cad56
Stephen <sk.gaming.1337@protonmail.com>
2021-11-23 17:30:02 -0500

    Add NotFound page

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import NotFoundImage from '../../assets/not-found.gif';
-
 const NotFound = () => {
   return (
     <div className='flex flex-col gap-5'>
@@ -9,13 +7,6 @@ const NotFound = () => {
         <h1 className='font-bold text-base md:text-2xl'>
           We couldn't find what you were looking for 😥
         </h1>
-      </div>
-      <div className='flex justify-center'>
-        <img
-          src={NotFoundImage}
-          alt='not-found'
-          className='h-60 md:h-96 w-full object-scale-down'
-        />
       </div>
     </div>
   );
